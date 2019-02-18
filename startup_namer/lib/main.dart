469443479bc55720738416ef51e4109572cdce6f
Mehedi Hasan Nirob <nirob@Mehedis-MacBook-Pro.local>
2019-02-18 21:17:30 +0600

    Added new project - startup_namer

---
+++ startup_namer/lib/main.dart
@@ -0,0 +1,22 @@
+//This example creates a Material app. Material is a visual design language that is standard on mobile and the web. Flutter offers a rich set of Material widgets
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
