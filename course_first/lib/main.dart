e75a88b940704118b74019216c5f64d8cae8142f
Mehedi Hasan Nirob <nirob@Mehedis-MacBook-Pro.local>
2019-02-26 01:22:31 +0600

    Added new project from udemy course

---
+++ course_first/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    new Center(
+      child: new Text('This is course first app', textDirection: TextDirection.ltr,),
+    )
+  );
+}
+
+
