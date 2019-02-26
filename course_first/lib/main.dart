24c187cc9d410c2003ca8fd59b0f81fb61df8d9c
Mehedi Hasan Nirob <nirob@Mehedis-MacBook-Pro.local>
2019-02-26 17:55:02 +0600

    Stateless widget added

--- course_first/lib/main.dart
+++ course_first/lib/main.dart
@@ -2,10 +2,28 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(
-    new Center(
-      child: new Text('This is course first app', textDirection: TextDirection.ltr,),
-    )
+    new application()
   );
 }
+
+class application extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Stateless widget',
+      home: new Scaffold(
+        body: new Container(
+          color: Colors.red,
+          child: new Container(
+            color: Colors.blue,
+            margin: const EdgeInsets.all(30.0),
+
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 
 
