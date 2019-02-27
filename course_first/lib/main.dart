b83f6975ba31c74ba44bfa26b51a098a2dbffa95
Mehedi Hasan Nirob <nirob@Mehedis-MacBook-Pro.local>
2019-02-27 23:28:11 +0600

    Added stateful widget

--- course_first/lib/main.dart
+++ course_first/lib/main.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(
-    new application()
+      //new Center(
+      //  child: new Text('This is course first app', textDirection: TextDirection.ltr,),
+      //)
+    new applicationS()
   );
 }
 
@@ -17,8 +20,43 @@ class application extends StatelessWidget {
           child: new Container(
             color: Colors.blue,
             margin: const EdgeInsets.all(30.0),
-
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class applicationS extends StatefulWidget {
+  @override
+  _applicationSState createState() => _applicationSState();
+}
+
+class _applicationSState extends State<applicationS> {
+
+  String ttext = '';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    ttext = 'initial : click this';
+    super.initState();
+
+  }
+
+  void method(){
+    setState(() {
+      ttext = 'clicked ;)';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Stateful Widget',
+      home: new Scaffold(
+        body: new Center(
+          child: new RaisedButton(onPressed: (){method();}, child: new Text(ttext),),
         ),
       ),
     );
