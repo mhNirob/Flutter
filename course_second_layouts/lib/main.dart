a5d941229f42cdc295a2dc3e667c899512cc598d
Mehedi Hasan Nirob <nirob@Mehedis-MacBook-Pro.local>
2019-03-05 16:04:25 +0600

    Grid Widget

--- course_second_layouts/lib/main.dart
+++ course_second_layouts/lib/main.dart
@@ -1,4 +1,5 @@
 
+/*
 import 'package:flutter/material.dart';
 
 
@@ -148,7 +149,7 @@ class _State extends State<listWidgetArray> {
 }
 
 
-/*
+
 class cardWidget extends StatefulWidget {
   @override
   _cardWidgetState createState() => _cardWidgetState();
@@ -174,3 +175,52 @@ class _cardWidgetState extends State<cardWidget> {
 
 
 */
+
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(new MaterialApp(home: new Application()));
+}
+
+class Application extends StatefulWidget {
+  @override
+  _ApplicationState createState() => new _ApplicationState();
+}
+
+class _ApplicationState extends State<Application> {
+
+  List<int> _items = new List();
+
+  @override
+  void initState() {
+    for(int i=0;i<50;i++){
+      _items.add(i);
+      print(i);
+    }
+    print(_items);
+    super.initState();
+  }
+
+
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        body: new GridView.builder(
+            gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4,),
+            itemCount: _items.length,
+            itemBuilder: (BuildContext context,int index){
+              return cards(index);
+            }
+        )
+    );
+  }
+}
+
+Widget cards(val){
+  return new Card(
+    color: Colors.pink,
+    child:new Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: new Text('$val'),
+    ),);
+}
