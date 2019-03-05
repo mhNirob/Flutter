3f42d3e37fea5683fa183ace8b55888333203b0f
Mehedi Hasan Nirob <nirob@Mehedis-MacBook-Pro.local>
2019-03-05 15:38:39 +0600

    Stack widget added

--- course_second_layouts/lib/main.dart
+++ course_second_layouts/lib/main.dart
@@ -1,6 +1,31 @@
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(listWidgetArray());
+
+void main() {
+  runApp(new MaterialApp(home: new listWidgetArray()));
+}
+
+class Application extends StatefulWidget {
+  @override
+  _ApplicationState createState() => new _ApplicationState();
+}
+
+class _ApplicationState extends State<Application> {
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        body: new Stack(
+          //alignment: Alignment.center,
+          children: <Widget>[
+            new Card(color: Colors.pink,child:new Padding(padding: const EdgeInsets.all(200.0),)),
+            new Card(color: Colors.green,child:new Padding(padding: const EdgeInsets.all(100.0),)),
+            new Card(color: Colors.blue,child:new Padding(padding: const EdgeInsets.all(50.0),)),
+            new Card(color: Colors.yellow,child:new Padding(padding: const EdgeInsets.all(10.0),)),
+          ]
+          ,)
+    );
+  }
+}
 
 
 class application extends StatelessWidget {
@@ -123,5 +148,29 @@ class _State extends State<listWidgetArray> {
 }
 
 
+/*
+class cardWidget extends StatefulWidget {
+  @override
+  _cardWidgetState createState() => _cardWidgetState();
+}
+
+class _cardWidgetState extends State<cardWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: new Stack(
+        children: <Widget>[
+          new Card(color: Colors.green, child:new Padding(padding: const EdgeInsets.all(200.0),)),
+          new Card(color: Colors.yellow, child:new Padding(padding: const EdgeInsets.all(20.0),)),
+          new Card(color: Colors.red, child:new Padding(padding: const EdgeInsets.all(100.0),),),
+          new Card(color: Colors.blue, child:new Padding(padding: const EdgeInsets.all(150.0),)),
+          new Card(color: Colors.orange, child:new Padding(padding: const EdgeInsets.all(75.0),)),
+        ],
+      )
+
+    );
+  }
+}
 
 
+*/
