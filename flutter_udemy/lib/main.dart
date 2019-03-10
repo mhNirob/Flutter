55a9d1a657ff28f514ff268685d5ce3ab07f76a7
Mehedi Hasan Nirob <nirob@Mehedis-MacBook-Pro.local>
2019-03-10 16:29:33 +0600

    flutter_udemy - Added button

--- flutter_udemy/lib/main.dart
+++ flutter_udemy/lib/main.dart
@@ -2,20 +2,33 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-
 class MyApp extends StatelessWidget {
-  @override 
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
-        ),
-        body: Card(child: Column(children: <Widget>[
-          Image.asset('assets/food.jpg'),
-          Text('Food paradise'),
-        ],),),
-      ),
+          appBar: AppBar(
+            title: Text('EasyList'),
+          ),
+          body: Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('Add Product'),
+                ),
+              ),
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset('assets/food.jpg'),
+                    Text('Food paradise'),
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
