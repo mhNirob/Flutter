edf58323910c2f08e97a6c1df31784f44d520f1e
Mehedi Hasan Nirob <nirob@Mehedis-MacBook-Pro.local>
2019-03-10 22:02:37 +0600

    flutter_udemy - Created stateful widget and managed data inside widget

--- flutter_udemy/lib/main.dart
+++ flutter_udemy/lib/main.dart
@@ -2,7 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,13 +28,19 @@ class MyApp extends StatelessWidget {
                   child: Text('Add Product'),
                 ),
               ),
-              Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/food.jpg'),
-                    Text('Food paradise'),
-                  ],
-                ),
+              Column(
+                children: _products
+                    .map(
+                      (elements) => Card(
+                            child: Column(
+                              children: <Widget>[
+                                Image.asset('assets/food.jpg'),
+                                Text(elements),
+                              ],
+                            ),
+                          ),
+                    )
+                    .toList(),
               ),
             ],
           )),
