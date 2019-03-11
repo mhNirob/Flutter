2b7ca7eb5384d05396c17f705f5121709205b7d6
Mehedi Hasan Nirob <nirob@Mehedis-MacBook-Pro.local>
2019-03-11 16:43:41 +0600

    flutter_udemy - Pass data to stateful widget

--- flutter_udemy/lib/product_manager.dart
+++ flutter_udemy/lib/product_manager.dart
@@ -3,12 +3,19 @@ import 'package:flutter/material.dart';
 import './products.dart';
 
 class ProductManager extends StatefulWidget {
+  final String startingProduct;
+  ProductManager(this.startingProduct);
   @override
   _ProductManagerState createState() => _ProductManagerState();
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = ['Food Tester'];
+  List<String> _products = [];
+  @override
+  void initState() {
+    super.initState();
+    _products.add(widget.startingProduct);    
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
