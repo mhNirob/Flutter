fd242a0cdd2800515cdc0997642bb7decac8f2a5
Mehedi Hasan Nirob <nirob@Mehedis-MacBook-Pro.local>
2019-03-14 16:39:05 +0600

    flutter_udemy - Passing data up

--- flutter_udemy/lib/main.dart
+++ flutter_udemy/lib/main.dart
@@ -8,6 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.deepOrange,
+        accentColor: Colors.deepPurple
+      ),
       home: Scaffold(
           appBar: AppBar(
             title: Text('EasyList'),
--- flutter_udemy/lib/product_manager.dart
+++ flutter_udemy/lib/product_manager.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './products.dart';
+import './product_control.dart';
 
 class ProductManager extends StatefulWidget {
   final String startingProduct;
@@ -14,22 +15,22 @@ class _ProductManagerState extends State<ProductManager> {
   @override
   void initState() {
     super.initState();
-    _products.add(widget.startingProduct);    
+    _products.add(widget.startingProduct);
   }
+
+  void _addProduct(String product) {
+    setState(() {
+      _products.add(product);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         Container(
           margin: EdgeInsets.all(10.0),
-          child: RaisedButton(
-            onPressed: () {
-              setState(() {
-                _products.add('Ultra Advanced Frood Tester');
-              });
-            },
-            child: Text('Add Product'),
-          ),
+          child: ProductControl(_addProduct),
         ),
         Products(_products),
       ],
